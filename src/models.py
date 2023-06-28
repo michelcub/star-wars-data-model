0723f5d67672304c5ed72125f8a7e3e2c0400c52
michelcub <orestelopez96@gmail.com>
2023-06-28 22:28:22 +0100

    add table

--- src/models.py
+++ src/models.py
@@ -67,7 +67,28 @@ class Vehicle_Properties(Base):
     created=Column(DateTime, nullable=False)
     edited= Column(DateTime, nullable=False) 
 
+class Character(Base):
+    __table_name__ = 'Character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    url = Column(String)
 
+
+class Character_Properties(Base):
+    id = Column(Integer, primary_key=True)
+    character_id = Column(ForeignKey('Character.id'))
+    cargo_capacity = Column(String, nullable=False)
+    consumables = Column(String, nullable=False)
+    crew = Column(String, nullable=False)
+    cargo_manufacturercapacity = Column(String, nullable=False)
+    max_atmosphering_speed = Column(String, nullable=False)
+    pilots = Column(String, nullable=False)
+    passengers = Column(String, nullable=False)
+    films = Column(String, nullable=False)
+    vehicle_class = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+    created = Column(DateTime)
+    edited = Column(DateTime)
 
 ## Draw from SQLAlchemy base
 render_er(Base, "diagram.png")
